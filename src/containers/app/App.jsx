2c379fd109cea2fd6c81480b47a1a86dcb9f6148
JulietShaban <julietshaban@gmail.com>
2023-01-23 16:19:57 +0200

    added deploy scripts

--- src/containers/app/App.jsx
+++ src/containers/app/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { REPO_NAME } from "@constants/repo";
 import Header from "@components/Header/Header";
 import routesConfig from "@routes/routesConfig";
 import styles from "./App.module.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={`/${REPO_NAME}/`}>
       <div className={styles.wrapper}>
         <Header />
         <Routes>
